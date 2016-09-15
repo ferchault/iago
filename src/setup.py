772c32f2a8dc9e31c777a423332a00589f2e6130
ferchault <guido@vonrudorff.de>
2016-09-15 16:12:26 +0100

    Setup travis and coveralls

---
+++ src/setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+import sys
+import os
+
+sys.path.append(os.path.abspath('..'))
+
+setup(name='iago',
+      version='0.dev',
+      description='Implicit Archiver for Generated Observables',
+      author='Guido Falk von Rudorff',
+      author_email='guido@vonrudorff.de',
+      url='https://github.com/ferchault/iago',
+      packages=['iago', ],
+      test_suite="tests",
+      license='MIT',
+      classifiers=['Development Status :: 3 - Alpha', ],
+      )
