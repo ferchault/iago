07f40df003b6a529e7097fc2b68a076e2f0db002
ferchault <guido@vonrudorff.de>
2016-09-15 15:33:01 +0100

    Updated analyser

--- src/iago/Analyser.py
+++ src/iago/Analyser.py
@@ -1,22 +1,98 @@
 import StorageProvider
 import pandas as pd
-
+import types
+import itertools as it
+import utils
+import DatabaseProvider
 
 class Analyser(object):
 	def __init__(self):
-		self.storage_provider = StorageProvider.JSONProvider()
+		self._db = DatabaseProvider.DB()
 
 	def setup(self):
 		""" Points out the required settings and parsers. Has to be user-defined."""
 		raise NotImplementedError()
 
 	def calculated_columns(self):
-		""" Holds the definitions of calculated columns as derived from the parsed result. Has to be user-defined."""
-		raise NotImplementedError()
+		""" Holds the definitions of calculated columns as derived from the parsed result. May be user-defined."""
+		pass
+
+	def define_groups(self):
+		""" Holds the definitions of static atom groups. May be user-defined."""
+		pass
+
+	def static_load_groups(self, filename):
+		try:
+			lines = open(filename).readlines()
+		except:
+			raise ValueError('Unable to read %s', filename)
+
+		self._db.groups.update(utils.parse_ndx(lines))
+
+	def static_group(self, *args):
+		if len(args) < 1:
+			raise RuntimeError('No empty groups allowed.')
+
+		first, rest = args[:1], args[1:]
+
+		# only dict given as argument
+		if len(args) == 1:
+			self._db.groups.update(first)
+
+		# selector string or list of elements
+		if len(args) == 2:
+			if isinstance(args[1], types.StringTypes):
+				self._parser.get_atom_indices(args[1])
+			else:
+				self._db.groups.update({first: args[1]})
+
+		# list of elements
+		self._db.groups.update({first: rest})
+
+	def dynamic_plane(self, name, selector, normal=None, comment=None):
+		""" Not suitable for wrapped trajectories.
+		:param name: Name of the plane.
+		:param selector: Which atoms are to be approximated by a plane.
+		:param normal: Preferred orientation of the normal vector. 3-tuple.
+		:param comment: Description of the plane.
+		:return:
+		"""
+		try:
+			maxidx = max(self._db.planes.index)
+		except ValueError:
+			maxidx = 0
+		for run in self.parser.get_runs():
+			try:
+				u = self.parser.get_universe(run)
+			except:
+				continue
+
+			steps = self.parser.get_trajectory_frames(run)
+
+			for step, frame in it.izip(steps, u.trajectory):
+				ag = u.selectAtoms(selector)
+				normal_vector, center_of_geometry = utils.fit_plane(ag.positions, normal=normal)
+				self._db.planes.loc[maxidx] = {
+					'run': run,
+					'frame': step,
+					'name': name,
+					'normal_x': normal_vector[0],
+					'normal_y': normal_vector[1],
+					'normal_z': normal_vector[2],
+					'support_x': center_of_geometry[0],
+					'support_y': center_of_geometry[1],
+					'support_z': center_of_geometry[2],
+				}
+				maxidx += 1
+
+
+
+
 
 	def run(self):
 		self.setup()
 		self.parser.run()
+		self.define_groups()
 		self.calculated_columns()
 		return self.storage_provider.save(self._db)
 
--- src/iago/DatabaseProvider.py
+++ src/iago/DatabaseProvider.py
@@ -1,12 +1,66 @@
 import pandas as pd
+import utils
+import json
+import pint
 
 
-class FrameBase(pd.DataFrame):
-	#: The kinetic energy in Hartree
-	ekin = None
-	#: The potential energy in Hartree
-	epot = None
+class DB(object):
+	def __init__(self):
+		ureg = pint.UnitRegistry()
 
+		self._groups = utils.SafeDict()
+		self.planes = utils.AnnotatedDataFrame({
+			'run': ('Run', None),
+			'frame': ('Frame number', None),
+			'name': ('Plane name', None),
+			'normal_x': ('Normal vector: x component', None),
+			'normal_y': ('Normal vector: y component', None),
+			'normal_z': ('Normal vector: z component', None),
+			'support_x': ('Support point: x component', ureg.angstrom),
+			'support_y': ('Support point: y component', ureg.angstrom),
+			'support_z': ('Support point: z component', ureg.angstrom),
+		})
+
+	@property
+	def groups(self):
+		""" Known static atom groups.
+		:return: Dictionary with group names as key, list of 0-based atom indices as value.
+		"""
+		return self._groups
+
+	def write(self, fh):
+		""" Writes the database to disk or stream.
+		:param fh: File handle or filename.
+		:return:
+		"""
+		if not hasattr(fh, 'write'):
+			fh = open(fh, 'w')
+
+		data = dict()
+
+		# groups
+		data['groups'] = self.groups
+		# planes
+		data['planes'] = self.planes.to_dict()
+		data['planes-meta'] = self.planes.annotations_to_dict()
+
+		# finalise
+		fh.write(json.dumps(data, separators=(',',':')))
+
+	def read(self, fh):
+		""" Reads the database from disk or stream.
+		:param fh: File handle or filename.
+		:return:
+		"""
+		if not hasattr(fh, 'write'):
+			fh = open(fh, 'w')
+
+		data = json.load(fh)
+
+		# groups
+		self._groups = utils.SafeDict(data['groups'])
+		# planes
+		self.planes = pd.DataFrame(data['planes-meta'], data['planes'])
 
 class Map(dict):
 	def __init__(self, *args, **kwargs):
