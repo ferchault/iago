b9a0542212f0f1b63edbe4bc5ea6cd6f90cf4501
ferchault <guido@vonrudorff.de>
2017-02-27 09:30:17 +0000

    Detecting and reading CP2K XYZ files, #8

--- src/iago/Reader.py
+++ src/iago/Reader.py
@@ -236,9 +236,15 @@ class CP2KReader(Reader):
 
 		# load universe
 		if topname is not None and configname is not None:
-			outputfile = os.path.join(self._path, self._config.MOTION.PRINT.TRAJECTORY.FILENAME + '-pos-1.dcd')
 			try:
+				format = self._config.MOTION.PRINT.TRAJECTORY.FORMAT.upper()
+			except:
+				format = 'XMOL'  # default from CP2K manual
+			fileextensions = {'DCD': 'dcd', 'DCD_ALIGNED_CELL': 'dcd', 'PDB': 'pdb', 'XMOL': 'xyz', 'XYZ': 'xyz'}
+			try:
+				outputfile = os.path.join(self._path, self._config.MOTION.PRINT.TRAJECTORY.FILENAME + '-pos-1.' + fileextensions[format])
 				self._universe = mda.Universe(topname, outputfile)
+
 			except OSError:
 				self._universe = EmptyUniverse()
 
