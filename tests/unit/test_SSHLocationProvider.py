2a3a237729608afce7167e4f204f28b4d2f9a7c7
ferchault <guido@vonrudorff.de>
2017-03-02 15:18:04 +0000

    Better test coverage for SSH locations

--- tests/unit/test_SSHLocationProvider.py
+++ tests/unit/test_SSHLocationProvider.py
@@ -1,6 +1,7 @@
 from unittest import TestCase
 from iago import LocationProvider as lp
 import paramiko
+import os
 
 class TestSSHLocationProvider(TestCase):
     def test__prepare_connect(self):
@@ -11,5 +12,22 @@ class TestSSHLocationProvider(TestCase):
         self.assertEqual(username, 'user')
         self.assertEqual(hostname, 'host')
         self.assertEqual(basepath, 'dir')
-
         self.assertIsInstance(client, paramiko.SSHClient)
+
+        path = 'foobar:dir'
+        print os.getcwd()
+        fh = open('.ssh_config', 'w')
+        fh.write('Host foobar\n\tUser snafu\n\tHostName foobar.example.com\n')
+        fh.close()
+        client, port, sock, username, hostname, basepath = lp.SSHLocationProvider._prepare_connect(path)
+        self.assertEqual(port, 22)
+        self.assertEqual(username, 'snafu')
+        self.assertEqual(hostname, 'foobar.example.com')
+        self.assertEqual(basepath, 'dir')
+        os.remove('.ssh_config')
+
+        self.assertRaises(ValueError, lp.SSHLocationProvider._prepare_connect, 'foobar')
+
+        self.assertRaises(ValueError, lp.SSHLocationProvider._prepare_connect, 'unknown_host:dir')
+
+
